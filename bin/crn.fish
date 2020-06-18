bd32341f5d35ba350fedc99b11edfa6903fe01c6
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2020-06-18 13:38:10 +0200

    wip

--- bin/crn.fish
+++ bin/crn.fish
@@ -1,10 +1,10 @@
 function crn
     set pth $dev/notes
-    set files $files $pth/category-theory.md
-    set files $files $pth/computer-sciences.md
+    set files $files $pth/category-theory.org
+    set files $files $pth/computer-sciences.org
     set files $files $pth/logics.org
     set files $files $pth/math.org
     set files $files $pth/notes.org
-    set files $files $pth/math-structures.md
+    set files $files $pth/math-structures.org
     cheat-grep --grep-args="$argv" --files="$files"
 end
